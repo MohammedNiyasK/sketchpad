f3d41620b1bf1f13328c3d6f10e51c46e91e3975
Mohammed Niyas <niyask1997@gmail.com>
2023-10-27 16:37:03 +0530

    environment checking added

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://sketchpad-server-mbo7.onrender.com";
+
+export const socket = io(URL);
